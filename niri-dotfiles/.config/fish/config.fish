ef89a498b67afd2890cf8a8e1bc43b3705715c4d
Mugzx <158086960+Mugzx@users.noreply.github.com>
2026-02-05 03:28:31 +0800

    chore: 为复杂配置添加文档链接

--- niri-dotfiles/.config/fish/config.fish
+++ niri-dotfiles/.config/fish/config.fish
@@ -8,16 +8,16 @@ starship init fish | source
 zoxide init fish --cmd cd | source
 
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
 
 function ls
-	command eza $argv
+    command eza $argv
 end
 
 thefuck --alias | source
--- niri-dotfiles/.config/scripts/quicksave.sh
+++ niri-dotfiles/.config/scripts/quicksave.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-snapper -c root create --description "quicksave"  --cleanup-algorithm number
+snapper -c root create --description "quicksave" --cleanup-algorithm number
 snapper -c root cleanup number
 snapper -c home create --description "quicksave" --cleanup-algorithm number
 snapper -c home cleanup number
